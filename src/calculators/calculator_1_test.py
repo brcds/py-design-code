24ba9e78badbd5e4b39242fcb23737e0d68c7b5a
brcds <brunocristianocontato@gmail.com>
2025-07-07 10:48:11 -0300

    finish tests calculator 1

--- src/calculators/calculator_1_test.py
+++ src/calculators/calculator_1_test.py
@@ -1,5 +1,4 @@
-import pytest
-
+from pytest import raises
 from typing import Dict
 from .calculator_1 import Calculator1
 
@@ -13,8 +12,6 @@ def test_calculate():
     mock_request = MockRequest(body={"number": 1})
     calculator_1 = Calculator1()
     response = calculator_1.calculate(mock_request)
-    print()
-    print(response)
 
     # formato da resposta
     assert "data" in response
@@ -24,3 +21,13 @@ def test_calculate():
     # Assertividade da Resposta
     assert response["data"]["result"] == 14.25
     assert response["data"]["Calculator"] == 1
+
+
+def test_calculate_with_body_error():
+    mock_request = MockRequest(body={"something": "1"})
+    calculator_1 = Calculator1()
+
+    with raises(Exception) as excinfo:
+        calculator_1.calculate(mock_request)
+
+    assert str(excinfo.value) == "Body mal formatado!"
