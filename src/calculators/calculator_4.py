122afe2344ab54fd491224af1e17cfcf067cde7e
brcds <brunocristianocontato@gmail.com>
2025-10-27 23:49:45 -0300

    edit calculator 4

--- src/calculators/calculator_4.py
+++ src/calculators/calculator_4.py
@@ -7,33 +7,18 @@ from src.errors.http_unprocessable_entity import HttpUnprocessableEntityError
 
 
 class Calculator4:
-    def calculate(self, request: FlaskRequest) -> dict:  # type: ignore
-        body = request.json
-        input_data = self.__validate_body(body)
-        media = self.__calculate_media(input_data)
-        formated_response = self.__format_response(input_data, media)
-        return formated_response
+    def calculate_average(self, numbers: list) -> dict:  # type: ignore
+        if not numbers or not all(isinstance(num, (int, float)) for num in numbers):
+            raise HttpUnprocessableEntityError("Lista de números não fornecida ou mal formatada!")
 
-    def __validate_body(self, body: Dict) -> list[float]:
-        if "numbers" not in body:
-            raise HttpUnprocessableEntityError("Body mal formatado!")
+        average = sum(numbers) / len(numbers)
+        response = self.__format_response(average)
+        return response
 
-        input_data = body["numbers"]
-
-        if not isinstance(input_data, list):
-            raise HttpBadRequestError("Informe uma lista de números!")
-
-        if len(input_data) < 2:
-            raise HttpBadRequestError("Informe pelo menos 2 números para calcular a média!")
-
-        return input_data
-
-    def __calculate_media(self, numbers: List[float]) -> float:
-        sum_numbers = sum(numbers)
-        len_numbers = len(numbers)
-        media = sum_numbers / len_numbers
-        return media
-
-    def __format_response(self, numbers: list, media: float) -> Dict:
-        media = "{:.2f}".format(media)
-        return {"data": {"Calculator": 4, "media": media, "numbers": numbers, 'Success': True}}
+    def __format_response(self, average: float) -> dict:
+        return {
+            "data": {
+                "Calculator": 4,
+                "average": round(average, 2)
+            }
+        }
--- src/calculators/calculator_4_test.py
+++ src/calculators/calculator_4_test.py
@@ -1,16 +1,7 @@
 from .calculator_4 import Calculator4
-from typing import Dict, List
-from pytest import raises
-
-
-class MockRequest:
-    def __init__(self, body: Dict) -> None:
-        self.json = body
 
 
 def test_calculate_average():
-    mock_request = MockRequest(body={"numbers": [4, 8, 8, 4]})
     calculator_4 = Calculator4()
-    response = calculator_4.calculate(mock_request)
-    assert response == {'data': {'Calculator': 4, 'media': '6.00', 'numbers': [4, 8, 8, 4], 'Success': True}}
-
+    response = calculator_4.calculate_average([4, 8, 8, 4])
+    assert response == {'data': {'Calculator': 4, 'average': 6.0}}
